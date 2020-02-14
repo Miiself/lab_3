76864ada32b65b3154c4f3e06a5820b6402a5f34
Miiself <joca7903@colorado.edu>
2020-02-13 23:11:49 -0700

    fixed email results problem, this completes the lab

--- lab3_script.sh
+++ lab3_script.sh
@@ -10,6 +10,6 @@ grep $exp $filename
 grep -c -P '^[0-9]{3}-[0-9]{3}-[0-9]{4}' "regex_practice.txt"
 grep -c -P '[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}' "regex_practice.txt"
 grep -o -P '^303-[0-9]{3}-[0-9]{4}$' "regex_practice.txt"
-grep -o -P '[a-zA-Z0-9._%+-]+@gerocities\.com' "regex_practice.txt">>email_results.txt
+grep -o -P '[a-zA-Z0-9._%+-]+@geocities\.com' "regex_practice.txt" >> email_results.txt
 git add email_results.txt
 git commit -m "these are the results"
